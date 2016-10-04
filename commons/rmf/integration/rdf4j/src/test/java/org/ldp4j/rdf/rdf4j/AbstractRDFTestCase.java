91eb114a9ae693910c80ad0ee42cd9c38819265a
krooijmans <koenrooymans@gmail.com>
2016-10-04 20:05:52 +0200

    LDP4J added RDF4J integration module.

---
+++ commons/rmf/integration/rdf4j/src/test/java/org/ldp4j/rdf/rdf4j/AbstractRDFTestCase.java
@@ -0,0 +1,80 @@
+/**
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ *   This file is part of the LDP4j Project:
+ *     http://www.ldp4j.org/
+ *
+ *   Center for Open Middleware
+ *     http://www.centeropenmiddleware.com/
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ *   Copyright (C) 2014-2016 Center for Open Middleware.
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *             http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ *   Artifact    : org.ldp4j.commons.rmf:integration-rdf4j:0.2.1
+ *   Bundle      : integration-rdf4j-0.2.1.jar
+ * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+ */
+package org.ldp4j.rdf.rdf4j;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.sail.SailRepository;
+import org.eclipse.rdf4j.sail.memory.MemoryStore;
+import org.junit.After;
+import org.junit.Before;
+
+public abstract class AbstractRDFTestCase {
+
+	private Repository repository;
+	private RepositoryConnection connection;
+	
+	@Before
+	public void setUpConnection() throws Exception {
+		repository = new SailRepository(new MemoryStore());
+		repository.initialize();
+		connection = repository.getConnection();
+	}
+	
+	@After
+	public void tearDownConnection() throws Exception {
+		if(connection!=null) {
+			try {
+				connection.close();
+			} finally {
+				repository.shutDown();
+			}
+		}
+	}
+
+	public final RepositoryConnection getConnection() {
+		return connection;
+	}
+
+	public final void dumpTurtle(String result) throws IOException {
+		BufferedReader reader=new BufferedReader(new StringReader(result));
+		boolean eol=false;
+		int i=0;
+		while(!eol) {
+			String line=reader.readLine();
+			if(line==null) {
+				eol=true;
+			} else {
+				System.out.println(String.format("[%03d]\t%s",++i,line));
+			}
+		}
+	}
+}
